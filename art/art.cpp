98e00b6faa82648183ad79b7128728f92d9ae9d7
nullwijn <56175027+nullwijn@users.noreply.github.com>
2020-02-06 21:46:19 -0500

    new clock

--- art/art.cpp
+++ art/art.cpp
@@ -2,29 +2,36 @@
 #include <cstdlib>
 #include <random>
 #include <fstream>
+#include <chrono>
 
 using namespace std;
 
+default_random_engine dre (chrono::steady_clock::now().time_since_epoch().count());
+
+int rng()
+{
+    uniform_int_distribution<> distr(33, 90);
+    return distr(dre);
+}
 int main()
 {
+    int x = 0;
+
     ofstream outFile;
-    
-    random_device rd;
-    mt19937 eng(rd());
-    uniform_int_distribution<> distr(65, 90);
 
     outFile.open("art.txt");
 
-    for(int x = 0; x < 25; x++)
+    for (int x = 0; x < 25; x++)
     {
         for(int x = 0; x < 25; x++)
         {
+          char b = char(rng());
+          outFile << b << " ";
             
-            char b = char(distr(eng));
-            outFile << b << " ";
         }
         outFile << endl;
     }
+
     outFile.close();
     return 0;
 }
