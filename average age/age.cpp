9c96172f1ab11e439d6942dba3cf063f130e4612
nullwijn <56175027+nullwijn@users.noreply.github.com>
2020-02-19 02:23:42 -0500

    mostly done

--- average age/age.cpp
+++ average age/age.cpp
@@ -2,10 +2,26 @@
 #include <iomanip>
 
 using namespace std;
-
+//TODO: make an array, append it within the loop, calculate average of elements in the array.
+double calc(int a)
+{
+    int count;
+    for(int x = 0; x < a; x++)
+    {
+        int age;
+        cout << "Enter age #" << count << ": ";
+        cin >> age;
+        count++;
+    }
+}
 int main()
 {
-    
+    int a;
+
+    cout << "Average Age Calculator\nEnter the amount of people: ";
+    cin >> a;
+
+    calc(a);
 
     return 0;
 }
